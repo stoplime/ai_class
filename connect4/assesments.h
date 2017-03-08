945b8ea0a35ad29adf44f008b0d12b077ce5b334
stoplime <slanimero@gmail.com>
2017-03-08 15:31:23 -0500

    bad stuff happened

--- connect4/assesments.h
+++ connect4/assesments.h
@@ -1,27 +1,46 @@
 // Evaluates the next choices
 #ifndef _ASSESMENTS_H_
 #define _ASSESMENTS_H_
-#include <vector>
+#include <array>
+#include <algorithm>
 #include <cassert>
+#include "grid.h"
 
 namespace steffen_space{
-    char empty_value = '.';
-    int connect_length = 4;
+    template<int width = 7, int height = 6, int connect = 4>
+    class assesment
+    {
+    private:
+        char empty_value = '.';
+    public:
+        grid_state<width, height> current_grid;
+        assesment();
+        ~assesment();
 
-    /// Utility assesment on an (x,y)
-    /// (x, y) is new piece
-    /// turn is the X or O turn
-    /// grid is the board being evaluated
-    template<class type>
-    int utility(std::vector< std::vector<type> > grid, int x, int y, type turn);
+        /// Utility assesment on an (x,y)
+        /// (x, y) is new piece
+        /// turn is the X or O turn
+        /// grid is the board being evaluated
+        float utility(std::array<std::array<char, height>, width> grid, int x, int y, char turn);
+    
+    };
+
 /*
-    short int expected_value(){
-        
-    }
+    /// calculate the best valued choise based on iterative utility checks
+    /// grid is the current board
+    /// turn is choice being optimized
+    template<class type>
+    int expected_value(std::vector< std::vector<type> > grid, type turn){
+        // state_space contains a tree like structure of all the possible choices on the grid starting with turn
+        state_space states;
 
+    }
+    
     short int heuristics(){
         
     }
     */
 }
+
+#include "assesment.template"
 #endif
--- connect4/main.cpp
+++ connect4/main.cpp
@@ -1,9 +1,8 @@
 // Steffen Lim Connect 4 algorithm
-// g++ -std=c++11 -g -Wall -o assignment2 main.cpp -I./headers
+// g++ -std=c++11 -g -Wall -o assignment2 main.cpp
 #include "tree_node.h"
-#include "grid.h"
 #include "assesments.h"
-#include "assesment.template"
+#include "grid.h"
 #include <iostream>
 #include <string>
 #include <cassert>
@@ -11,27 +10,28 @@
 using namespace std;
 using namespace steffen_space;
 
+assesment<> board;
+
 // function prototypes
-void get_user_settings(grid_state& grid);
-void player_turn(grid_state& grid);
-void oponent_turn(grid_state& grid);
+void get_user_settings();
+void player_turn();
+void oponent_turn();
 
 // main
 int main(int argc, char** argv){
-    grid_state board;
-    get_user_settings(board);
+    get_user_settings();
     while (1)
     {
-        cout << board.to_string();
-        player_turn(board);
-        cout << board.to_string();
-        oponent_turn(board);
+        cout << board.current_grid.to_string();
+        player_turn();
+        cout << board.current_grid.to_string();
+        oponent_turn();
     }
     
 }
 
 // function implementations
-void get_user_settings(grid_state& grid){
+void get_user_settings(){
     string board_width;
     string board_height;
     string connet_length;
@@ -43,23 +43,23 @@ void get_user_settings(grid_state& grid){
     cout << "Connect length:";
     cin >> connet_length;
 
-    grid = grid_state(stoi(board_width),stoi(board_height),stoi(connet_length));
+    board = assesment<stoi(board_width),stoi(board_height),stoi(connet_length)>();
 }
 
-void player_turn(grid_state& grid){
+void player_turn(){
     string input;
-    cout << "Player input(1-" << grid.get_grid().size() << "): ";
+    cout << "Player input(1-" << board.current_grid.get_grid().size() << "): ";
     cin >> input;
-    size_t input_height = grid.set_data('X', stoi(input)-1);
-    int result = utility<char>(grid.get_grid(), stoi(input)-1, (int)input_height, 'X');
+    size_t input_height = board.current_grid.set_data('X', stoi(input)-1);
+    float result = utility<char>(board.current_grid.get_grid(), stoi(input)-1, (int)input_height, 'X');
     cout << "result: " << result << endl;
 }
 
 void oponent_turn(grid_state& grid){
     string input;
-    cout << "Oponent input(1-" << grid.get_grid().size() << "): ";
+    cout << "Oponent input(1-" << board.current_grid.get_grid().size() << "): ";
     cin >> input;
-    size_t input_height = grid.set_data('O', stoi(input)-1);
-    int result = utility<char>(grid.get_grid(), stoi(input)-1, (int)input_height, 'O');
+    size_t input_height = board.current_grid.set_data('O', stoi(input)-1);
+    float result = utility<char>(board.current_grid.get_grid(), stoi(input)-1, (int)input_height, 'O');
     cout << "result: " << result << endl;
 }

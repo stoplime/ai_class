58e799328c59ce5e7f940ff6b8b440c50b8fc988
stoplime <slanimero@gmail.com>
2017-04-20 14:05:29 -0400

    adjusted for play

--- connect4/main.cpp
+++ connect4/main.cpp
@@ -17,7 +17,7 @@ using namespace steffen_space;
 int width = 7;
 int height = 6;
 int connect = 4;
-int ai_depth1 = 5;
+int ai_depth1 = 8;
 int ai_depth2 = 5;
 int ai_weight1 = 0;
 int ai_weight2 = 1;
@@ -39,99 +39,101 @@ int main(int argc, char** argv){
     board.set_max_depth(ai_depth1);
     // cout << board.get_grid_state().to_string();
     pre_state = board.get_grid_state();
-    int bound_weight = 0;
-    int weight_types = 8;
+
+    // int bound_weight = 0;
+    // int weight_types = 8;
+
     // for (int i = 0; i < weight_types; i++){
     //     cout << "weight " << i << endl;
     //     board.update_weights(i);
     // }
 
-    // bool result = false;
-    // board.update_weights(7);
-    // // board.set_max_depth(7);
-    // while (!result){
-    //     // board.update_weights(i);
-    //     // board.set_max_depth(d1);
-    //     // bool result = player_turn(board, 'X', pre_state); /*
-    //     result = ai_X_turn(board); //*/
-    //     cout << board.get_grid_state().to_string();
-    //     if (result){
-    //         cout << "✗ won!" << endl;
-    //         continue;
-    //     }
-    //     else if(++count_turns >= height*width){
-    //         cout << "Its a tie!" << endl;
-    //         result = true;
-    //         continue;
-    //     }
-        
-    //     // board.update_weights(j);
-    //     // board.set_max_depth(d2);
-    //     // result = ai_O_turn(board); /*
-    //     result = player_turn(board, 'O', pre_state); //*/
-    //     cout << board.get_grid_state().to_string();
-    //     if (result){
-    //         cout << "O won!" << endl;
-    //         continue;
-    //     }
-    //     else if(++count_turns >= height*width){
-    //         cout << "Its a tie!" << endl;
-    //         result = true;
-    //         continue;
-    //     }
-    // }// end game
-    // board.get_grid_state() = grid_state(width, height);
-    // count_turns = 0;
-    
-    for (int d1 = 2; d1 < 7; d1++){
-        for (int d2 = 2; d2 < 7; d2++){
-            // cout << "depth: " << d1 << ", " << d2 << endl;
-            for (int i = bound_weight; i < weight_types; i++){
-                for (int j = bound_weight; j < weight_types; j++){
-                    // cout << i << "\t" << j << endl;
-                    if (i != j){
-                        bool result = false;
-                        while (!result){
-                            board.update_weights(i);
-                            board.set_max_depth(d1);
-                            // bool result = player_turn(board, 'X', pre_state); /*
-                            result = ai_X_turn(board); //*/
-                            // cout << board.get_grid_state().to_string();
-                            if (result){
-                                // cout << "✗ won!" << endl;
-                                cout << i << "\t" << j << "\tX" << endl;
-                                continue;
-                            }
-                            else if(++count_turns >= height*width){
-                                // cout << "Its a tie!" << endl;
-                                result = true;
-                                continue;
-                            }
-                            
-                            board.update_weights(j);
-                            board.set_max_depth(d2);
-                            result = ai_O_turn(board); /*
-                            result = player_turn(board, 'O', pre_state); //*/
-                            // cout << board.get_grid_state().to_string();
-                            if (result){
-                                // cout << "O won!" << endl;
-                                cout << i << "\t" << j << "\tO" << endl;
-                                continue;
-                            }
-                            else if(++count_turns >= height*width){
-                                // cout << "Its a tie!" << endl;
-                                result = true;
-                                continue;
-                            }
-                        }// end game
-                        board.get_grid_state() = grid_state(width, height);
-                        count_turns = 0;
-                    }
-                }
-            }
-
+    bool result = false;
+    board.update_weights(7);
+    // board.set_max_depth(7);
+    while (!result){
+        // board.update_weights(i);
+        // board.set_max_depth(d1);
+        bool result = player_turn(board, 'X', pre_state); /*
+        result = ai_X_turn(board); //*/
+        cout << board.get_grid_state().to_string();
+        if (result){
+            cout << "✗ won!" << endl;
+            continue;
         }
-    }
+        else if(++count_turns >= height*width){
+            cout << "Its a tie!" << endl;
+            result = true;
+            continue;
+        }
+        
+        // board.update_weights(j);
+        // board.set_max_depth(d2);
+        result = ai_O_turn(board); /*
+        result = player_turn(board, 'O', pre_state); //*/
+        cout << board.get_grid_state().to_string();
+        if (result){
+            cout << "O won!" << endl;
+            continue;
+        }
+        else if(++count_turns >= height*width){
+            cout << "Its a tie!" << endl;
+            result = true;
+            continue;
+        }
+    }// end game
+    board.get_grid_state() = grid_state(width, height);
+    count_turns = 0;
+    
+    // for (int d1 = 2; d1 < 7; d1++){
+    //     for (int d2 = 2; d2 < 7; d2++){
+    //         // cout << "depth: " << d1 << ", " << d2 << endl;
+    //         for (int i = bound_weight; i < weight_types; i++){
+    //             for (int j = bound_weight; j < weight_types; j++){
+    //                 // cout << i << "\t" << j << endl;
+    //                 if (i != j){
+    //                     bool result = false;
+    //                     while (!result){
+    //                         board.update_weights(i);
+    //                         board.set_max_depth(d1);
+    //                         // bool result = player_turn(board, 'X', pre_state); /*
+    //                         result = ai_X_turn(board); //*/
+    //                         // cout << board.get_grid_state().to_string();
+    //                         if (result){
+    //                             // cout << "✗ won!" << endl;
+    //                             cout << i << "\t" << j << "\tX" << endl;
+    //                             continue;
+    //                         }
+    //                         else if(++count_turns >= height*width){
+    //                             // cout << "Its a tie!" << endl;
+    //                             result = true;
+    //                             continue;
+    //                         }
+                            
+    //                         board.update_weights(j);
+    //                         board.set_max_depth(d2);
+    //                         result = ai_O_turn(board); /*
+    //                         result = player_turn(board, 'O', pre_state); //*/
+    //                         // cout << board.get_grid_state().to_string();
+    //                         if (result){
+    //                             // cout << "O won!" << endl;
+    //                             cout << i << "\t" << j << "\tO" << endl;
+    //                             continue;
+    //                         }
+    //                         else if(++count_turns >= height*width){
+    //                             // cout << "Its a tie!" << endl;
+    //                             result = true;
+    //                             continue;
+    //                         }
+    //                     }// end game
+    //                     board.get_grid_state() = grid_state(width, height);
+    //                     count_turns = 0;
+    //                 }
+    //             }
+    //         }
+
+    //     }
+    // }
     
 }
 
@@ -193,30 +195,30 @@ bool player_turn(assesment& grid, char char_input, grid_state& prev){
 }
 
 bool ai_X_turn(assesment& grid){
-    // clock_t start;
-    // double duration;
-    // start = clock();
+    clock_t start;
+    double duration;
+    start = clock();
 
     int input = grid.build_state_space('X');
 
-    // duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    // cout << "duration: " << duration << " second(s)" << endl;
-    // cout << "AI X input: " << input << endl;
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    cout << "duration: " << duration << " second(s)" << endl;
+    cout << "AI X input: " << input << endl;
     int input_height = grid.get_grid_state().set_data('X', input);
     bool result = grid.utility(input, input_height);
     return result;
 }
 
 bool ai_O_turn(assesment& grid){
-    // clock_t start;
-    // double duration;
-    // start = clock();
+    clock_t start;
+    double duration;
+    start = clock();
 
     int input = grid.build_state_space('O');
 
-    // duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    // cout << "duration: " << duration << " second(s)" << endl;
-    // cout << "AI O input: " << input << endl;
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    cout << "duration: " << duration << " second(s)" << endl;
+    cout << "AI O input: " << input << endl;
     int input_height = grid.get_grid_state().set_data('O', input);
     bool result = grid.utility(input, input_height);
     return result;
